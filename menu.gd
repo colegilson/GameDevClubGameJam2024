39224ba4da6e7fead191dd4a52d347a722782e6e
colegilson <colegilson10@gmail.com>
2024-02-19 22:40:38 -0800

    basic non-appealing menu system created using control scenes

---
+++ menu.gd
@@ -0,0 +1,17 @@
+extends Control
+
+
+
+func _on_play_pressed():
+	get_tree().change_scene_to_file("res://level_select.tscn")
+
+
+
+func _on_options_pressed():
+	get_tree().change_scene_to_file("res://options.tscn")
+
+
+
+
+func _on_quit_pressed():
+	get_tree().quit()
